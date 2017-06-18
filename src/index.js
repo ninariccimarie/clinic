cf9a1b6abf67f714a057795973295f33bdc08134
Nina <ninariccimarie@gmail.com>
2017-06-18 21:53:20 +0800

    remove practice code

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import Calculator from './components/temperature/Calculator';
 import App from './App';
 import './index.css';
 
 
-//ReactDOM.render(<Calculator />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
